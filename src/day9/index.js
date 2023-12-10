793bdf7315200413a813dc32c4e2b0195d2f1b92
Preston Goforth <p@osano.com>
2023-12-10 09:50:41 -0500

    Day 9 Part 1

--- src/day9/index.js
+++ src/day9/index.js
@@ -7,7 +7,26 @@ const dir = path.join(dirname(fileURLToPath(import.meta.url)), 'input.txt')
 export const str = readFileSync(dir, 'utf8')
 
 export const part1 = async () => {
-    throw new Error('Not answered')
+    const extrapolations = str.split('\n').map((line) => {
+        const values = line.split(' ').map(Number)
+        const rows = [values]
+        let current = [...values]
+        while (current.length > 0 && current.some((value) => value !== 0)) {
+            current = current.reduce((acc, value, idx, list) => {
+                if (idx === 0) {
+                    return acc
+                }
+                const last = current[idx - 1]
+                return [...acc, value - last]
+            }, [])
+            rows.push(current)
+        }
+        return rows.reduce((acc, row) => {
+            const last = row[row.length - 1]
+            return acc + last
+        }, 0)
+    })
+    return extrapolations.reduce((acc, value) => acc + value, 0)
 }
 
 export const part2 = async () => {
